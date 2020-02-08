278dbb43365caa993ab5bbef477e578b6eff9cbc
Tony Bui <tony.bui.jp@gmail.com>
2020-02-08 20:13:26 +0900

    create question factory and seeding data

--- app/Question.php
+++ app/Question.php
@@ -14,6 +14,6 @@ class Question extends Model {
 
   public function setTitleAttribute($value) {
     $this->attributes['title'] = $value;
-    $this->attributes['slug'] = str_slug($value);
+    $this->attributes['slug'] = Str::slug($value);
   }
 }
