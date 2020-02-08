b6be29353d0931134f80ba33880fc5d3e916a6a8
tony.bui <tony.bui.jp@gmail.com>
2020-02-08 20:16:15 +0900

    create question factory and seeding data (#6)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,18 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run() {
+    factory(App\User::class, 5)->create()->each(function($user) {
+      $user->questions()
+           ->saveMany(
+             factory(App\Question::class, rand(1, 5))->make()
+           );
+    });
+  }
 }
