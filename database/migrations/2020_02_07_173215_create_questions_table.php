28aea46b8abd422439517907245fc8569958b742
tony.bui <tony.bui.jp@gmail.com>
2020-02-16 19:14:33 +0900

    create answer factory (#18)

--- database/migrations/2020_02_07_173215_create_questions_table.php
+++ database/migrations/2020_02_07_173215_create_questions_table.php
@@ -4,15 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuestionsTable extends Migration
-{
+class CreateQuestionsTable extends Migration {
   /**
    * Run the migrations.
    *
    * @return void
    */
-  public function up()
-  {
+  public function up() {
     Schema::create('questions', function (Blueprint $table) {
       $table->bigIncrements('id');
       $table->string('title');
@@ -34,8 +32,7 @@ class CreateQuestionsTable extends Migration
    *
    * @return void
    */
-  public function down()
-  {
+  public function down() {
     Schema::dropIfExists('questions');
   }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,11 +9,15 @@ class DatabaseSeeder extends Seeder {
    * @return void
    */
   public function run() {
-    factory(App\User::class, 5)->create()->each(function($user) {
+    factory(App\User::class, 5)->create()->each(function ($user) {
       $user->questions()
            ->saveMany(
              factory(App\Question::class, rand(1, 5))->make()
-           );
+           )
+           ->each(function ($question) {
+             $question->answers()
+                      ->saveMany(factory(App\Answer::class, rand(1,5))->make());
+           });
     });
   }
 }
